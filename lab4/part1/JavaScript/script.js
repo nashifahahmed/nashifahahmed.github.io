de6b55b07a68518b228544f292d29c202f6b7a94
nashifahahmed <nashifah.ahmed@dcmail.ca>
2026-04-07 14:30:19 -0400

    Update script.js
    initial variables and functions (created three arrays the holds the given strings in comments)

--- lab4/part1/JavaScript/script.js
+++ lab4/part1/JavaScript/script.js
@@ -18,17 +18,11 @@ function randomValueFromArray(array) {
 
 // Raw text strings
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
-// the soup kitchen
-// Disneyland
-// the White House
+const places = ["the soup kitchen", "Disneyland", "the White House"];
 
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
+const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
 
 // Partial return random string function
 
