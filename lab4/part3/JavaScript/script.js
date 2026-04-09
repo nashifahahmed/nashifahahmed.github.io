bb39b13f2a89be46d358aa455c759bf3bda04687
nashifahahmed <nashifah.ahmed@dcmail.ca>
2026-04-09 16:57:32 -0400

    Update script.js
    updated the ball data (update method + if statements in case ball goes off sides of canvas)

--- lab4/part3/JavaScript/script.js
+++ lab4/part3/JavaScript/script.js
@@ -25,7 +25,9 @@ function randomRGB() {
     return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
+// Ball class
 class Ball {
+    // Constructor (param = x y coordinates, velocity, color, size)
     constructor(x, y, velX, velY, color, size){
         this.x = x;
         this.y = y;
@@ -35,10 +37,44 @@ class Ball {
         this.size = size;
     }
 
+    // Draw Method
     draw() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
     }
+
+    // Update Method
+    update(){
+
+        // If the ball is going off the right edge
+        if(this.x + this.size >= width){
+            this.velX = -this.velX;
+        }
+
+        // If the ball is going off the left edge
+        if (this.x - this.size <= 0) {
+            this.velX = -this.velX;
+        }
+
+        // If the ball is going off the bottom edge
+        if(this.y + this.size >= height){
+            this.velY = -this.velY;
+        }
+
+        // If the ball is going off the top edge
+        if(this.y - this.size <= 0){
+            this.velY = -this.velY;
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
 }
+
+const testBall = new Ball(50, 100, 4, 4, "blue", 10);
+testBall.x;
+testBall.size;
+testBall.color;
+testBall.draw();
