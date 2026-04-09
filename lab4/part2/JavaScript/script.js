a3d438b7c0ec8e5a6915e9ab1f46cff11abeea36
nashifahahmed <nashifah.ahmed@dcmail.ca>
2026-04-09 15:22:37 -0400

    Update script.js
    added the image to the thumbnail bar (added url to image folder, for loop to loop through the images, events for clicking and hitting enter)

--- lab4/part2/JavaScript/script.js
+++ lab4/part2/JavaScript/script.js
@@ -11,6 +11,7 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
+// Array of objects - images
 const images = [
     {filename: "pic1.jpg", alt: "Closeup of a human eye"},
     {filename: "pic2.jpg", alt: "Rock that look like a wave"},
@@ -18,4 +19,28 @@ const images = [
     {filename: "pic4.jpg", alt: "Section of wall from a pharoah\'s tomb"},
     {filename: "pic5.jpg", alt: "Large moth on a leaf"}
 ];
+
+// URL for image files (folder containing images)
+const baseURL = "images/";
+
+// For loop to loop through images
+for (const image of images){
+    const newImage = document.createElement("img");
+
+    newImage.src = `${baseURL}${image.filename}`;
+
+    newImage.alt = image.alt;
+
+    newImage.tabIndex = "0";
+
+    thumbBar.appendChild(newImage);
+
+    newImage.addEventListener("click", updateDisplayedImage());
+
+    newImage.addEventListener("keydown", (e) => {
+        if (e.code == "Enter") {
+            updateDisplayedImage(e);
+        }
+    });
+}
 
